aa6fa1c1a1ae6b870bab25120f1467072c4d5914
diegorafaelalves <diegorafaelalves17@gmail.com>
2023-06-05 09:10:39 -0300

    tela de cadastro

---
+++ app/src/main/java/com/example/vidasaudavel/BancoDados.kt
@@ -0,0 +1,7 @@
+package com.example.vidasaudavel
+
+data class BancoDados(
+    var Cadastro: String? = null,
+    var Email: String? = null,
+    var Senha: String? = null
+)
--- app/src/main/java/com/example/vidasaudavel/MainActivity.kt
+++ app/src/main/java/com/example/vidasaudavel/MainActivity.kt
@@ -1,27 +1,88 @@
 package com.example.vidasaudavel
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.vidasaudavel.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var dbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        findViewById<TextView>(com.google.android.material.R.id.header_title).text = getString(R.string.Email)
-        findViewById<TextView>(com.google.android.material.R.id.header_title).text = getString(R.string.Senha)
 
+        val Cadastro = binding.Cadastro
+        val Email = binding.Email
+        val Senha = binding.Senha
 
-        binding.icNavegar.setOnClickListener{
-            val navegarTelaNavegacao = Intent(this,TelaNavegacao:: class.java)
-            startActivity(navegarTelaNavegacao)
+        dbRef = FirebaseDatabase.getInstance().getReference("Cadastro")
+
+        Cadastro.setOnClickListener {
+            val email = Email.text.toString()
+            val password = Senha.text.toString()
+
+            if (email.isEmpty()) {
+                Email.error = "Por favor insira seu e-mail"
+            }
+            if (password.isEmpty()) {
+                Senha.error = "Por favor insira sua senha"
+            } else {
+
+                val reg = dbRef.push().key!!
+                val Cadastro = BancoDados( Cadastro = null, Email = null, Senha = null)
+
+                dbRef.child(reg).setValue(Cadastro)
+                    .addOnCompleteListener {
+                        Toast.makeText(this, "Cadastro realizado", Toast.LENGTH_SHORT).show()
+
+                        Email.text?.clear()
+                        Senha.text?.clear()
+
+                    }
+                    .addOnFailureListener { err ->
+                        Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_SHORT).show()
+
+                    }
+            }
+        }
+                //val intent = Intent(this, MainActivity::class.java)
+                //startActivity(intent)
+
+            binding.icNavegar.setOnClickListener {
+                val navegarTelaNavegacao = Intent(this, TelaNavegacao::class.java)
+                startActivity(navegarTelaNavegacao)
+            }
+
         }
 
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
